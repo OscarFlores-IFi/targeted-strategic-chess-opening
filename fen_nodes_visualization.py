3c2ada6009fbed79a9017780c12829438424ff37
OscarFlores-IFi <if715029@gmail.com>
2024-03-17 12:36:32 +0100

    Works pretty well! :)

--- fen_nodes_visualization.py
+++ fen_nodes_visualization.py
@@ -24,11 +24,10 @@ from fentoimage.board import BoardImage
 from bokeh.io import output_file, show, save
 from bokeh.io import output_notebook, show, save
 from bokeh.models import Range1d, Circle, ColumnDataSource, MultiLine, HoverTool
-from bokeh.plotting import figure
-from bokeh.plotting import from_networkx
+from bokeh.plotting import figure, from_networkx
 from bokeh.palettes import Blues8, Reds8, Purples8, Oranges8, Viridis8, Spectral8, inferno
 from bokeh.transform import linear_cmap
-
+from bokeh.colors import RGB
 #%%
 # # path_to_file = 'lichess_db_standard_rated_2024-01.pgn'
 # path_to_file = 'lichess_DrNykterstein_2024-03-10.pgn'
@@ -106,11 +105,11 @@ from bokeh.transform import linear_cmap
 # BigDF.to_parquet('chess_games_large_fen_carlsen.parquet')
 
 #%% Pre-processing
-BigDF = pd.read_parquet('chess_games_large_fen_carlsen.parquet')
+BigDF = pd.read_parquet('chess_games_large_fen_oscarmex45.parquet')
 
 min_number_of_games = 30
 
-BigDF = BigDF[BigDF['player_white'] == 1]
+BigDF = BigDF[BigDF['player_white'] == 0]
 
 tmp = BigDF['moves'].value_counts()
 # get the most common chess positions
@@ -161,117 +160,20 @@ for ranking, fen_position in inverted_dict_of_common_fen_positions.items():
 connections_dict = {}
 for i in range(subset.shape[0]-1):
     if subset.loc[i]['id'] == subset.loc[i+1]['id']:
-        try:
-            connections_dict[(subset.loc[i]['moves_id'], subset.loc[i+1]['moves_id'])] += 1        
-        except KeyError:
-            connections_dict[(subset.loc[i]['moves_id'], subset.loc[i+1]['moves_id'])] = 1 
-    else:      
-        try:
-            connections_dict[(0, subset.loc[i+1]['moves_id'])] += 1        
-        except KeyError:
-            connections_dict[(0, subset.loc[i+1]['moves_id'])] = 1 
-
-   
-   
-#%% Color by count. undirected Graph. 
-
-
-# Create a graph object
-G = nx.Graph()
-
-# Add edges from the dictionary including weights
-for edge, weight in connections_dict.items():
-    G.add_edge(edge[0], edge[1], weight=weight)
-
-#Choose a title!
-title = 'Chess Openings Network'
-
-output_file(filename="custom_filename.html", title=title)
-nx.set_node_attributes(G, name='count_of_positions', values=counts_of_fen_positions)
-nx.set_node_attributes(G, name='log_counts', values = {i:np.log(j)*3 for i,j in counts_of_fen_positions.items()})
-
-#Establish which categories will appear when hovering over each node
-HOVER_TOOLTIPS = [("Ranking by frequency", "@index"),
-                  ("Count of positions", "@count_of_positions")]
-
-#Pick a color palette — Blues8, Reds8, Purples8, Oranges8, Viridis8
-color_palette = Blues8
-
-#Create a plot — set dimensions, toolbar, and title
-plot = figure(tooltips = HOVER_TOOLTIPS,
-              tools="pan,wheel_zoom,save,reset", active_scroll='wheel_zoom',
-            x_range=Range1d(-10.1, 10.1), y_range=Range1d(-10.1, 10.1), title=title, width= 1980, height=1080)
-
-#Create a network graph object with spring layout
-network_graph = from_networkx(G, nx.spring_layout, scale=10, seed=42,  center=(0, 0))
-
-#Set node sizes and colors according to node degree (color as spectrum of color palette)
-minimum_value_color = min(network_graph.node_renderer.data_source.data["count_of_positions"])
-maximum_value_color = max(network_graph.node_renderer.data_source.data["count_of_positions"])
-network_graph.node_renderer.glyph = Circle(size="log_counts", fill_color=linear_cmap("count_of_positions", color_palette, minimum_value_color, maximum_value_color))
-
-
-
-#Set edge opacity and width
-# network_graph.edge_renderer.glyph = MultiLine(line_alpha=0.5, line_width=1)
-
-
-#Add network graph to the plot
-plot.renderers.append(network_graph)
-
-show(plot)
-#save(plot, filename=f"{title}.html")
-
-#%% Directed Acyclic Graph
-from bokeh.colors import RGB
-
-# Generate or load your directed graph
-G = nx.DiGraph()
-
-# Add edges from the dictionary including weights
-for edge, weight in connections_dict.items():
-    G.add_edge(edge[0], edge[1], weight=weight)
-
-# Set node attributes
-# nx.set_node_attributes(G, name='count_of_positions', values=counts_of_fen_positions)
-nx.set_node_attributes(G, name='count_of_positions', values=counts_of_fen_positions)
-nx.set_node_attributes(G, name='log_counts', values={node: np.log(counts_of_fen_positions[node]) * 3 for node in G.nodes()})
-
-# Set up the output file and plot
-title = 'Chess Openings Network'
-output_file(filename="custom_filename.html", title=title)
-plot = figure(tools="pan,wheel_zoom,save,reset", active_scroll='wheel_zoom', x_range=Range1d(-10.1, 10.1), y_range=Range1d(-10.1, 10.1), title=title, width=1980, height=1080)
-
-# Establish tooltips
-HOVER_TOOLTIPS = [("Ranking by frequency", "@index"), ("Count of positions", "@count_of_positions")]
-
-# Create network graph object
-network_graph = from_networkx(G, nx.spring_layout, scale=10, center=(0, 0))
-
-
-
-# Set node sizes and colors according to node degree
-minimum_value_color = min(nx.get_node_attributes(G, 'count_of_positions').values())
-maximum_value_color = max(nx.get_node_attributes(G, 'count_of_positions').values())
-color_mapper = linear_cmap(field_name='count_of_positions', palette=color_palette, low=minimum_value_color, high=maximum_value_color)
-network_graph.node_renderer.glyph = Circle(size="log_counts", fill_color=color_mapper)
-
-# Set edge opacity and width
-# Example: network_graph.edge_renderer.glyph = MultiLine(line_alpha=0.5, line_width=1)
-
-# Add network graph to the plot
-plot.renderers.append(network_graph)
-
-# Add hover tool
-plot.add_tools(HoverTool(tooltips=HOVER_TOOLTIPS))
-
-# Show the plot
-show(plot)
-
+        if subset.loc[i]['index'] + 1 == subset.loc[i + 1]['index']:
+            try:
+                connections_dict[(subset.loc[i]['moves_id'], subset.loc[i+1]['moves_id'])] += 1        
+            except KeyError:
+                connections_dict[(subset.loc[i]['moves_id'], subset.loc[i+1]['moves_id'])] = 1 
+    else:
+        if subset.loc[i+1]['index'] == 0:
+            try:
+                connections_dict[(0, subset.loc[i+1]['moves_id'])] += 1        
+            except KeyError:
+                connections_dict[(0, subset.loc[i+1]['moves_id'])] = 1 
 
 #%%
-from bokeh.transform import linear_cmap
-from bokeh.colors import RGB
+
 
 # Define custom palette with intermediate colors
 red = RGB(255, 0, 0)
@@ -294,73 +196,27 @@ intermediate_colors2 = [RGB(int((1 - i) * yellow.r + i * lime.r),
                         for i in [0.2, 0.4, 0.6, 0.8]]
 
 # Create custom palette with all colors
-# custom_palette = [red] + intermediate_colors1 + [yellow] + intermediate_colors2 + [green]
-custom_palette = [red] + [yellow] + [green]
+custom_palette = [red] + intermediate_colors1 + [yellow] + intermediate_colors2 + [green]
+# custom_palette = [red] + [yellow] + [green]
 
 # Create color mapper with custom palette
 
-
-#%% Color by count. undirected Graph. 
+#%%from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource, HoverTool
 
 
 # Create a graph object
-G = nx.Graph()
+# G = nx.Graph()
+G = nx.DiGraph()
+
 
 # Add edges from the dictionary including weights
 for edge, weight in connections_dict.items():
     G.add_edge(edge[0], edge[1], weight=weight)
+    
+# Calculate node positions using a layout algorithm
+pos = nx.spring_layout(G)
 
-#Choose a title!
-title = 'Chess Openings Network'
-
-output_file(filename="custom_filename.html", title=title)
-nx.set_node_attributes(G, name='won_positions', values=wins_per_fen_position)
-nx.set_node_attributes(G, name='count_of_positions', values=counts_of_fen_positions)
-nx.set_node_attributes(G, name='win_ratio', values=win_ratio_per_fen_position)
-nx.set_node_attributes(G, name='lift', values=lift_per_fen_position)
-
-nx.set_node_attributes(G, name='log_counts', values = {i:np.log(j)*3 for i,j in counts_of_fen_positions.items()})
-
-#Establish which categories will appear when hovering over each node
-HOVER_TOOLTIPS = [("Ranking by frequency", "@index"),
-                  ("Count of positions", "@count_of_positions"),
-                  ("Count of won games", "@won_positions"),
-                  ("Win Ratio", "@win_ratio"),
-                  ("lift", "@lift")
-                  ]
-
-#Pick a color palette — Blues8, Reds8, Purples8, Oranges8, Viridis8
-color_palette = Blues8
-
-#Create a plot — set dimensions, toolbar, and title
-plot = figure(tooltips = HOVER_TOOLTIPS,
-              tools="pan,wheel_zoom,save,reset", active_scroll='wheel_zoom',
-            x_range=Range1d(-10.1, 10.1), y_range=Range1d(-10.1, 10.1), title=title, width= 1980, height=1080)
-
-#Create a network graph object with spring layout
-network_graph = from_networkx(G, nx.spring_layout, seed=42, scale=10, center=(0, 0))
-
-#Set node sizes and colors according to node degree (color as spectrum of color palette)
-minimum_value_color = min(network_graph.node_renderer.data_source.data["lift"])
-maximum_value_color = max(network_graph.node_renderer.data_source.data["lift"])
-color_mapper = linear_cmap(field_name='lift', palette=custom_palette, low=minimum_value_color, high=maximum_value_color)
-network_graph.node_renderer.glyph = Circle(size="log_counts", fill_color=color_mapper)
-
-
-
-#Set edge opacity and width
-network_graph.edge_renderer.glyph = MultiLine(line_alpha=0.5, line_width=1)
-
-
-#Add network graph to the plot
-plot.renderers.append(network_graph)
-
-show(plot)
-#save(plot, filename=f"{title}.html")
-
-
-#%%from bokeh.plotting import figure, show
-from bokeh.models import ColumnDataSource, HoverTool
 
 # Define tooltips with image and additional information for nodes
 TOOLTIPS_NODES = """
@@ -378,8 +234,6 @@ TOOLTIPS_NODES = """
     </div>
 """
 
-# Calculate node positions using a layout algorithm
-pos = nx.spring_layout(G)
 
 # Now, 'pos' is a dictionary where keys are node identifiers and values are (x, y) positions
 # You can access positions for specific nodes like this:
@@ -397,11 +251,16 @@ lift = [lift_per_fen_position[i] for i in G.nodes]
 # Calculate line coordinates for edges
 lines_x = []
 lines_y = []
+lines_weight = []
 for edge in G.edges():
     start = pos[edge[0]]
     end = pos[edge[1]]
     lines_x.append([start[0], end[0]])
     lines_y.append([start[1], end[1]])
+    try:
+        lines_weight.append(np.ceil(np.log(connections_dict[edge]*20))*10)
+    except KeyError:
+        lines_weight.append(np.ceil(np.log(connections_dict[(edge[1], edge[0])]*20))*10)
 
 # Create a sample ColumnDataSource with data and image URLs for nodes
 data_nodes = {
@@ -422,10 +281,19 @@ source_nodes = ColumnDataSource(data_nodes)
 p = figure(width=1980, height=1080, active_scroll='wheel_zoom')
 
 # Plot the lines for edges
-p.multi_line(lines_x, lines_y, line_color='#CCCCCC', line_width=1)
+p.multi_line(lines_x, lines_y, line_color=lines_weight, line_width=1)
+
+# Color by count of positions
+# minimum_value_color = min(counts_fen)
+# maximum_value_color = max(counts_fen)
+# color_mapper = linear_cmap(field_name='count_of_positions', palette=Blues8, low=minimum_value_color, high=maximum_value_color)
+
+minimum_value_color = max(min(lift),0.5)
+maximum_value_color = min(max(lift),1.5)
+color_mapper = linear_cmap(field_name='lift', palette=custom_palette, low=minimum_value_color, high=maximum_value_color)
 
 # Plot the points with images for nodes
-circle = p.circle('x', 'y', size='log_count_of_positions', source=source_nodes, fill_color='blue', line_color=None)
+circle = p.circle('x', 'y', size='log_count_of_positions', source=source_nodes, fill_color=color_mapper, line_color='black')
 
 # Define hover tool for circles (nodes)
 hover_tool_nodes = HoverTool(renderers=[circle], tooltips=TOOLTIPS_NODES)
